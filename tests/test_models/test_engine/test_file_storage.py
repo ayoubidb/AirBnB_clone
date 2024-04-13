0e77758f0245e97b5ac796dcca3ab1b21e44a603
ayoubidb <ayoubidb@gmail.com>
2024-04-13 08:40:00 -0700

    My first commit

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -96,7 +96,7 @@ class TestFileStorage(unittest.TestCase):
         """ tests __init__ with many arguments"""
         with self.assertRaises(TypeError) as error:
             base = FileStorage(7, 12)
-        fail = "object() takes no parameters"
+        fail = "FileStorage() takes no arguments"
         self.assertEqual(str(error.exception), fail)
 
     def test_all_dict_returned(self):
